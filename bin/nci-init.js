bf5a3f71c206f09fe6c96ba6f6e64b772e867944
gaoletian <gaoletian@finupgroup.com>
2019-04-17 19:51:00 +0800

    feat: run.sh 添加HOST环境变量

--- bin/nci-init.js
+++ bin/nci-init.js
@@ -1,66 +1,60 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const {
-    title,
-    success,
-    warn,
-    projectDir,
-    homeDir
-} = require('./lib/utils.js');
+const fs = require("fs");
+const { title, success, warn, projectDir, homeDir } = require("./lib/utils.js");
 
 // 生成 run.sh
 function genarateRun(filename) {
-    const fullPath = projectDir(filename)
-    if (fs.existsSync(fullPath)) {
-        return warn(fullPath + '已存在');
-    }
-    const content = `#!/usr/bin/env bash
-NODE_ENV=$env PORT=$PORT0 node main.js
-    `
-    fs.writeFileSync(fullPath, content);
-    success(fullPath)
+  const fullPath = projectDir(filename);
+  if (fs.existsSync(fullPath)) {
+    return warn(fullPath + "已存在");
+  }
+  const content = `#!/usr/bin/env bash
+NODE_ENV=$env PORT=$PORT0 HOST=0.0.0.0 node main.js
+    `;
+  fs.writeFileSync(fullPath, content);
+  success(fullPath);
 }
 
 // 生成项目配置
 function genarateNci(filename) {
-    const fullPath = projectDir(filename);
-    if (fs.existsSync(fullPath)) {
-        return warn(fullPath + ' 已存在 ')
-    }
-    const content = `{
+  const fullPath = projectDir(filename);
+  if (fs.existsSync(fullPath)) {
+    return warn(fullPath + " 已存在 ");
+  }
+  const content = `{
   "dockerHost": "harbor.finupgroup.com",
   "dockerGroup": "",
   "baseImage": "harbor.finupgroup.com/nchannel/node:alinode",
   "appName": ""
-}`
-    fs.writeFileSync(fullPath, content);
-    success(fullPath)
+}`;
+  fs.writeFileSync(fullPath, content);
+  success(fullPath);
 }
 
 function genarateGlobalNci(filename) {
-    const fullPath = homeDir(filename);
-    if (fs.existsSync(fullPath)) {
-        warn(fullPath + ' 已存在 ')
-        return;
-    }
-    const content = `{
+  const fullPath = homeDir(filename);
+  if (fs.existsSync(fullPath)) {
+    warn(fullPath + " 已存在 ");
+    return;
+  }
+  const content = `{
   "dockerHost": "harbor.finupgroup.com",
   "dockerGroup": "",
   "baseImage": "harbor.finupgroup.com/nchannel/node:alinode",
   "dockerUser": "",
   "dockerPass": ""
-}`
+}`;
 
-    fs.writeFileSync(fullPath, content);
-    success(fullPath)
+  fs.writeFileSync(fullPath, content);
+  success(fullPath);
 }
 
 function init() {
-    title('初始化配置')
-    genarateNci('nci.json');
-    genarateRun('run.sh');
-    genarateGlobalNci('.ncirc.json');
+  title("初始化配置");
+  genarateNci("nci.json");
+  genarateRun("run.sh");
+  genarateGlobalNci(".ncirc.json");
 }
 
-init()
+init();
